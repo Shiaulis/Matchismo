f1c7b430fd22b7c2488734b96d6b7bb9dfd36b10
Andrius Shiaulis <shiaulis@gmail.com>
2024-05-26 09:20:05 +0300

    Assignment 1 solution

--- Matchismo/CardGameViewController.h
+++ Matchismo/CardGameViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  CardGameViewController.h
 //  Matchismo
 //
 //  Created by Andrius Shiaulis on 26.05.2024.
@@ -7,7 +7,7 @@
 
 @import UIKit;
 
-@interface ViewController : UIViewController
+@interface CardGameViewController : UIViewController
 
 
 @end
--- Matchismo/CardGameViewController.m
+++ Matchismo/CardGameViewController.m
@@ -1,20 +1,34 @@
 //
-//  ViewController.m
+//  CardGameViewController.m
 //  Matchismo
 //
 //  Created by Andrius Shiaulis on 26.05.2024.
 //
 
-#import "ViewController.h"
+#import "CardGameViewController.h"
+#import "PlayingCardDeck.h"
 
-@interface ViewController ()
+@interface CardGameViewController ()
+
+@property (strong, nonatomic) Deck *deck;
 
 @property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
 @property (nonatomic) int flipCount;
 
 @end
 
-@implementation ViewController
+@implementation CardGameViewController
+
+#pragma - Setters and getters -
+
+- (Deck *)deck
+{
+    if (_deck == nil) {
+        _deck = [PlayingCardDeck new];
+    }
+
+    return _deck;;
+}
 
 - (void)setFlipCount:(int)flipCount
 {
@@ -23,8 +37,12 @@
     NSLog(@"flipCount = %d", self.flipCount);
 }
 
+#pragma - IBActions -
+
 - (IBAction)touchCardButton:(UIButton *)sender
 {
+    Card *card = [self.deck drawRandomCard];
+
     if (sender.currentTitle.length != 0) {
         [sender setBackgroundImage:[UIImage imageNamed:@"cardback"]
                           forState:UIControlStateNormal];
@@ -33,7 +51,7 @@
     else {
         [sender setBackgroundImage:[UIImage imageNamed:@"cardfront"]
                           forState:UIControlStateNormal];
-        [sender setTitle:@"A♣️" forState:UIControlStateNormal];
+        [sender setTitle:card.contents forState:UIControlStateNormal];
     }
 
     self.flipCount += 1;
